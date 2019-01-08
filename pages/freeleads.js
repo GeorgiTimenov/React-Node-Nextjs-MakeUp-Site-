a5f572ed8949861d04a074ca716627da1566e58e
Mko Galoyan <mko@flayr.com.au>
2019-01-08 16:29:23 +1100

    added redirect

---
+++ pages/freeleads.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Router from 'next/router'
+export default class extends React.Component {
+  static async getInitialProps({ res }) {
+    if (res) {
+      res.writeHead(302, {
+        Location: 'https://flayr-io-mua-signup.paperform.co/'
+      })
+      res.end()
+    } else {
+      Router.push('https://flayr-io-mua-signup.paperform.co/')
+    }
+    return {}
+  }
+}
