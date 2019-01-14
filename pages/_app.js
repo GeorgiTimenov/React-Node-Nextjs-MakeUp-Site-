491a7a6c8bedc37c3814e7c5cdb0777bc531dea3
Mko Galoyan <mko@flayr.com.au>
2019-01-14 16:23:54 +1100

    added domain verification

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ export default class MyApp extends App {
         <title>FLAYR | Mobile Makeup Artists & Hair Stylists</title>
         <meta name="description" content="Mobile Hair And Makeup.Hairdressers,Hair Stylists &amp; Makeup Artists For A Special Occassion Or For The Latest Bridal Makeup. Free Travel To You. Prices From $60."></meta>   
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="w6QWVWHl2veXw8C82BBK7PzJ8WUAxNVeOFXH6W199Tg" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <link rel="shortcut icon" href="//cdn.shopify.com/s/files/1/1665/4771/files/favicon_flayr_mobile_makeupartists_and_hair_stylists_32x32.png?v=1536803770" type="image/png" />
         </Head>
